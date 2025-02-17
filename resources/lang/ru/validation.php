2dbb578b730e60fe3d0738dc228c9b55ca69d876
DevDuulat <duulat.bekm@mail.ru>
2025-02-17 07:58:21 +0300

    added validation in Blade and bug localization

---
+++ resources/lang/ru/validation.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'required' => 'Поле :attribute обязательно для заполнения.',
+    'min' => [
+        'string' => 'Поле :attribute должно содержать минимум :min символа.',
+    ],
+    'max' => [
+        'string' => 'Поле :attribute не должно превышать :max символов.',
+    ],
+    'regex' => 'Поле :attribute имеет неверный формат.',
+    'in' => 'Выбранное значение для :attribute недопустимо.',
+    'integer' => 'Поле :attribute должно быть числом.',
+    'date' => 'Поле :attribute должно быть корректной датой.',
+    'after_or_equal' => 'Поле :attribute должно быть не раньше сегодняшней даты.',
+
+    'attributes' => [
+        'name' => 'Имя',
+        'phone' => 'Телефон',
+        'reservation_date' => 'Дата бронирования',
+        'time' => 'Время',
+        'table_number' => 'Номер столика',
+        'number_of_persons' => 'Количество персон',
+        'location' => 'Локация',
+        'wishes' => 'Пожелания',
+    ],
+];
+
+
