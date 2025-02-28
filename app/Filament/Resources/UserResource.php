2e36fe7a1e4bf3a648d502eab29fa98e32e2fa01
DevDuulat <duulat.bekm@mail.ru>
2025-02-28 17:39:41 +0300

    remove is admin for UserResource.php

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -38,8 +38,7 @@ class UserResource extends Resource
                     ->password()
                     ->required()
                     ->maxLength(255),
-                Forms\Components\Toggle::make('is_admin')
-                    ->required(),
+
             ]);
     }
 
