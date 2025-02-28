e60d9d4d25884f8d2ee0fa770a5a408586e900ae
DevDuulat <duulat.bekm@mail.ru>
2025-02-28 15:11:07 +0300

    remove hidden class for link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -235,7 +235,7 @@
                             3500
                         </div>
                     </div>
-                    <div class="hidden md:block md- text-center">
+                    <div class="md:block md- text-center">
                         <a href="{{ route('menu.index') }}"
 
                            class="text-white bg-yellow-500 px-4 py-2 rounded-2xl uppercase font-semibold hover:bg-yellow-700 ">Меню</a>
